4cc183836d5b9be25a84195923d49b9c035b9d3a
acui <xinfeng.cui@foxmail.com>
2020-07-08 17:28:31 +0800

    -

---
+++ array_learn/SquareNonDecre.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+
+public class SquareNonDecre {
+    public static int[] sortedSquares(int[] A){
+        for(int i = 0; i < A.length; i++){
+            A[i] *= A[i];
+        }
+        Arrays.sort(A);
+        return A;
+    }
+
+    public static void main(String[] args) {
+        int[] A = {-4,-1,1,3,5};
+        sortedSquares(A);
+        for(int num : A){
+            System.out.println(num);
+            
+        }
+    }
+}
