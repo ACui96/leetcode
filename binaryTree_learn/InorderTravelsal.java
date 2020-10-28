1727bd42e92704b251f42314bf733b4a0ef664ed
acui <acui962018@gmail.com>
2020-10-28 08:27:47 +0800

    InorderTravelsal

---
+++ binaryTree_learn/InorderTravelsal.java
@@ -0,0 +1,16 @@
+public class InorderTravelsal {
+    public Lis<Integer> inorderTravelsal(TreeNode rootNode) {
+        List<Integer> res = new ArrayList<Integer>();
+        inorder(root, res);
+        return res;
+    }
+
+    public void inorder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+}
